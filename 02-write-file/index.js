2bcb5924eb1be5f57a3a8343a2e69ebc02c548ee
Julia Filippova <julia.94@bk.ru>
2022-05-15 01:07:58 +0300

    feat: task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const { stdin, stdout } = require('process');
+
+fs.writeFile(
+  path.join(__dirname, 'text.txt'), '',
+  (err) => {
+    if (err) throw err;
+  }
+);
+stdout.write('Write your message, to finish press Ctrl+C or enter "exit"\n');
+stdin.on('data', data => {
+  if (data.toString().includes('exit')) {
+    console.log('Goodbye!!!');
+    process.exit(); 
+  }
+  fs.appendFile(
+    path.join(__dirname, 'text.txt'),
+    data,  
+    (err) => {
+      if (err) throw err;
+    },
+  );
+});
+process.on('SIGINT', () => {
+  console.log('Goodbye!!!');
+  process.exit();
+});
