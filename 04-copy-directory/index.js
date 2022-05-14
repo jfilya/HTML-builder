10abb6a9d24d1eb8c38a6b48b0f1f9c9a9ea6278
Julia Filippova <julia.94@bk.ru>
2022-05-15 01:08:58 +0300

    feat: task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {//создаем папку
+  if (err) throw err;
+  fs.readdir(path.join(__dirname, 'files-copy'), (_err, copy) => {// читаем содержимое новой созданной папки
+    for(let j=0; j<copy.length; j++){
+      fs.unlink(path.join(__dirname, 'files-copy', copy[j]), err => {//удаляем ее содержимое
+        if(err) throw err;
+      });
+    }
+  });
+  fs.readdir(path.join(__dirname, 'files'), (_err, items) => {// читаем содержимое старой папки
+    for (let i=0; i<items.length; i++) {
+      fs.copyFile(path.join(__dirname, 'files', items[i]), path.join(__dirname, 'files-copy', items[i]), err => {//копируем содержимое старой папки в новую
+        if(err) throw err;
+      });
+    }
+  });
+});
+
+
 
